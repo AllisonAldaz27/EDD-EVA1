309a5d200d8b0709ca87d65c9d7c9e0287576901
AllisonAldaz27 <124476993+AllisonAldaz27@users.noreply.github.com>
2023-02-21 09:29:23 -0600

    Add files via upload

---
+++ EVA1_7_METODOS1/src/eva1_7_metodos1/EVA1_7_METODOS1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_7_metodos1;
+
+public class EVA1_7_METODOS1 {
+
+    public static void main(String[] args) {
+myMethod();//invocar al metodo;llamada a funcion
+    
+    }
+    public static void myMethod(){
+        System.out.println("You have called me : My name is myMethod!!");
+    }
+}
