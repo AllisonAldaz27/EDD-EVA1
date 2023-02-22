1fc740f8996cb70c73a5d5cbf714b862003f5a85
AllisonAldaz27 <124476993+AllisonAldaz27@users.noreply.github.com>
2023-02-22 09:27:53 -0600

    Add files via upload

---
+++ EVA1_11_RETURN/src/eva1_11_return/EVA1_11_RETURN.java
@@ -0,0 +1,17 @@
+package eva1_11_return;
+public class EVA1_11_RETURN {
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+        int resu;
+         resu= square(10);//invocacion o llamada a funcion
+         System.out.println("RESULTADO:  "+ resu);
+    }
+    public static int square(int num){
+        return num*num;
+        
+    }
+    
+    
+    
+}
