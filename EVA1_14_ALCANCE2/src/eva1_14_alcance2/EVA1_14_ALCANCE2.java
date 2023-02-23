b95b4ad213c3dba077c5643804ecdfbbb0ebd3b5
AllisonAldaz27 <124476993+AllisonAldaz27@users.noreply.github.com>
2023-02-23 09:45:28 -0600

    Add files via upload

---
+++ EVA1_14_ALCANCE2/src/eva1_14_alcance2/EVA1_14_ALCANCE2.java
@@ -0,0 +1,21 @@
+
+package eva1_14_alcance2;
+
+public class EVA1_14_ALCANCE2 {
+
+    public static void main(String[] args) {//las llaves son los bloques 
+        // TODO code application logic here
+   int x=100; //visible dentro del bloque main    
+   for (int i = 0; i < 10; i++) {
+       int x=100;   
+       int y=100;//visible dentro del for 
+       if (i==5){
+           
+           
+           
+            }
+        }
+   int y=100;//aqui la variable y del ciclo for no existe ,podemos declarar una variable del mismo nombre
+    }
+    
+}
